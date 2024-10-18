30cb2d1e13f117d84e709993ce9c03a474a35bd4
MansourWorkingSpace <mansour.tech.contact@gmail.com>
2024-10-18 12:19:49 +0100

    styles

--- covoiturage/src/app/navbar/navbar.component.ts
+++ covoiturage/src/app/navbar/navbar.component.ts
@@ -1,13 +1,20 @@
+import { NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,NgStyle],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  bg_color={'background-color':'white'};
+  bg_inscri(){
+    this.bg_color={'background-color':'#F4F6FF'};
+  }
+  bg_accueil(){
+    this.bg_color={'background-color':'white'};
+  }
 }
