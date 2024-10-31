69d0ecf35da28cb9becf81c88bc3b8c9098e9b00
MansourWorkingSpace <mansour.tech.contact@gmail.com>
2024-10-31 13:01:15 +0100

    responsive navbar and addition of styles

--- covoiturage/src/app/app.component.ts
+++ covoiturage/src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { NavbarComponent } from "./navbar/navbar.component";//import forms module
-import { BgImageComponent } from './bg-image/bg-image.component';
-
+import { Component, NgModule } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FormsModule, NgControl, NgModel } from '@angular/forms';
+import { NavbarComponent } from "./navbar/navbar.component";
+import { ConnexionComponent } from "./connexion/connexion.component";
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, NavbarComponent,BgImageComponent],//import forms module
+  imports: [RouterOutlet, FormsModule,NgIf, NavbarComponent, ConnexionComponent],
   exportAs: 'appRoot',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
--- covoiturage/src/app/navbar/navbar.component.ts
+++ covoiturage/src/app/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { InscriptionComponent } from '../inscription/inscription.component';
+import { ConnexionComponent } from '../connexion/connexion.component';
 
 @Component({
   selector: 'app-navbar',
